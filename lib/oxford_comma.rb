5945294fd357ca289dbe06dc4b727160d26d0247
Richard Kurwitz <kurwitz3@aol.com>
2020-08-19 00:59:17 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -5,3 +5,8 @@ elsif array.length == 3
 array.join(","" " "and")
 end
 end
+def oxford_comma(list)
+  return list.join(' and ') if list.size < 3
+  list[-1] = "and " + list[-1]
+  list.join(', ')
+end
